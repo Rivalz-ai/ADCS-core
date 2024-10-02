926e86214028a6379ea93e2e03cfe79702536d03
doclt <thongdoc0508@gmail.com>
2024-10-02 17:10:56 +0700

    created struture for contracts

---
+++ contracts/src/mock/MockERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../interface/IERC20Mintable.sol";
+
+contract MockERC20 is ERC20("MockERC20", "MOCK"), IERC20Mintable {
+    function mint(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+}
