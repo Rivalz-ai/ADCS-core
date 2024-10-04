114b9971c476efdfee83003b6150c9618b7c1a90
KelvinThai <quan.thai1987@gmail.com>
2024-10-04 17:10:49 +0700

    created ADCS consumer base, coordinator base, ADCS coordinator

--- contracts/src/mock/MockERC20.sol
+++ contracts/src/mock/MockERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../interface/IERC20Mintable.sol";
+import "../interfaces/IERC20Mintable.sol";
 
 contract MockERC20 is ERC20("MockERC20", "MOCK"), IERC20Mintable {
     function mint(address recipient, uint256 amount) external {
