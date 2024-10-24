628edceccd5ef1eeb21b4d2ab90c2688bd951463
doclt <thongdoc0508@gmail.com>
2024-10-25 06:51:42 +0700

    addev version for inference api

--- open/src/inference/inference.controller.ts
+++ open/src/inference/inference.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
 import { InferenceService } from './inference.service'
 
-@Controller('inference')
+@Controller({ path: 'inference', version: '1' })
 export class InferenceController {
   constructor(private inferenceService: InferenceService) {}
 
