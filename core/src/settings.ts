e2d676d9b97fd878e1cd36bd25294cec2b784e39
Toan Nguyen <toannguyen@Toans-Mac-mini.local>
2025-03-25 15:29:29 +0700

    Deploy core service

--- core/src/settings.ts
+++ core/src/settings.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 import { JsonRpcProvider } from 'ethers'
-dotenv.config()
+dotenv.config({ path: '/app/.env' })
 
 export const CHAIN = process.env.CHAIN || 'localhost'
 export const DEPLOYMENT_NAME = process.env.DEPLOYMENT_NAME || 'rivalz'
@@ -52,6 +52,8 @@ export const LOG_DIR = process.env.LOG_DIR || './log'
 
 export const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
 export const REDIS_PORT = process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379
+export const REDIS_USERNAME = process.env.REDIS_USERNAME || 'default'
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || ''
 export const PROVIDER_URL = process.env.PROVIDER_URL || 'http://127.0.0.1:8545'
 export const API_URL = process.env.API_URL || 'http://localhost:3010'
 export const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL || ''
@@ -135,3 +137,13 @@ export const PROVIDER = createJsonRpcProvider()
 export function getObservedBlockRedisKey(contractAddress: string) {
   return `${contractAddress}-listener-${DEPLOYMENT_NAME}`
 }
+
+console.log(process.env.REDIS_HOST)
+console.log(process.env.REDIS_PASSWORD)
+console.log(process.env.REDIS_USERNAME)
+console.log(process.env.REDIS_PORT)
+console.log(process.env.PROVIDER_URL)
+console.log(process.env.API_URL)
+console.log(process.env.ADCS_API_URL)
+console.log(process.env.CHAIN)
+console.log(process.env.LOG_DIR)
