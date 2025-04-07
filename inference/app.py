ac2b5e79114b8d46aa97816c6d3808ba89fd24fa
Ussama-1 <devixbug@gmail.com>
2025-04-08 01:13:34 +0500

    add-asi1-mini-model-as-provider

--- inference/app.py
+++ inference/app.py
@@ -8,6 +8,8 @@ from blueprints.GeminiPro import geminipro_bp
 from blueprints.GeminiAdvanced import geminiadvanced_bp
 from blueprints.GeminiUltr import geminiultr_bp
 from blueprints.Qwen import qwen_bp
+from  blueprints.asi1 import asi1_bp
+
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -22,6 +24,8 @@ app.register_blueprint(geminiflash8b_bp, url_prefix='/geminiflash8b')
 app.register_blueprint(geminipro_bp, url_prefix='/geminipro')
 app.register_blueprint(geminiadvanced_bp, url_prefix='/geminiadvanced')
 app.register_blueprint(qwen_bp, url_prefix='/Qwen')
+app.register_blueprint(asi1_bp, url_prefix="/asi1")
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='localhost',port=3003)
