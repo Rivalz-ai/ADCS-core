4be0631c2b24f02fb1102b65e761217c8cc26a25
doclt <thongdoc0508@gmail.com>
2025-04-24 15:23:20 +0700

    Add API key management functionality to AuthController and AuthService
    - Introduced endpoints for generating, retrieving, and deleting API keys in AuthController.
    - Updated AuthService to handle API key generation and management, including expiration settings.
    - Enhanced JWT token generation to include user ID in the payload.
    - Added JWT_API_EXP configuration for API key expiration duration.
    - Cleaned up console log statements in app.settings.ts for consistency.

--- open/src/auth/auth.controller.ts
+++ open/src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Post, Body, UnauthorizedException, Param } from '@nestjs/common'
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger'
+import {
+  Controller,
+  Post,
+  Body,
+  UnauthorizedException,
+  Param,
+  UseGuards,
+  Req,
+  Get,
+  Delete
+} from '@nestjs/common'
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { VerifySignatureDto } from './dto/verify.dto'
+import { JwtAuthGuard } from './auth.guard'
+import { Request } from 'express'
 
 @ApiTags('Authorization')
 @Controller({ path: 'auth', version: '1' })
@@ -28,5 +40,38 @@ export class AuthController {
       throw new UnauthorizedException('Invalid signature')
     }
     return { accessToken: token }
+  }
+
+  @Post('api-key')
+  @ApiOperation({ summary: 'Generate an API key for a user' })
+  @ApiResponse({ status: 200, description: 'Returns an API key for the user' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT')
+  async generateApiKey(@Req() req: Request) {
+    return {
+      message: 'success',
+      data: await this.authService.generateApiKey(req.user['address'], req.user['userId'])
+    }
+  }
+
+  @Get('api-keys')
+  @ApiOperation({ summary: 'Get all API keys for a user' })
+  @ApiResponse({ status: 200, description: 'Returns all API keys for the user' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT')
+  async getApiKeys(@Req() req: Request) {
+    return { message: 'success', data: await this.authService.apiKeys(req.user['userId']) }
+  }
+
+  @Delete('api-key/:id')
+  @ApiOperation({ summary: 'Delete an API key for a user' })
+  @ApiResponse({ status: 200, description: 'Returns the deleted API key' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT')
+  async deleteApiKey(@Param('id') id: string, @Req() req: Request) {
+    return {
+      message: 'success',
+      data: await this.authService.deleteApiKey(parseInt(id), req.user['userId'])
+    }
   }
 }
